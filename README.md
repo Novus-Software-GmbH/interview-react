7bd8d4ef1380b4abbbdc5195c7f16ddaeefd382f
Razvan Chisu <r.chisu@novus-software.eu>
2025-03-11 08:10:01 +0100

    initial

--- README.md
+++ README.md
@@ -23,11 +23,11 @@ We have a main page with a menu on the left (Home, Buildings, Flats) and a conte
 
 # Your tasks
 
-1. Have a look at MainContent.tsx -> would you change anything? 
+1. Have a look at MainContent.tsx -> would you change anything? Can you fix the timer?
 2. Have a look at Buildings.tsx. Would you change anything?
 3. Show the list of flats from `http://localhost:3000/flats` when clicking on "Flats" in the main menu. Optional: add sorting and pagination.
 4. Add a possibility for editing an existing flat and POST'ing it to `http://localhost:3000/flats` (the endpoint is not implemented in the mock-server and you need not implement it. Just send the request.)
-5. The form should have basic validation (flat area should be a positive number; the flat must belong to a building)
+5. Make sure flat area is a positive number and the flat belongs to a building.
 6. Add authentication to all requests you send to the mock server (Authorization: 'Bearer xyz') (auth is not implemented in the mock server and you need not implement it. Just add the header to your requests)
 7. Handle all error responses from the mock backend (4xx, 5xx), e.g. by logging them to the console
 
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,24 +1,34 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import styles from './MainContent.module.css';
 import Buildings from './Buildings';
 import logo from '../assets/zeag_wowiconsult_logo+topjob-logo.jpg';
 
-const Home = () => (
-  <div className={styles.page} style={{float:'left', width:'100%'}}>
+const Home = () => {
+  
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+  }, []);
+
+  return (  
+  <div className={styles.page} style={{ float: 'left', width: '100%' }}>
     <h2>Hallo Bewerber!</h2>
     <p>Willkommen bei wowi consult. Wir freuen uns darauf, zu sehen, was du so kannst :)</p>
-    <p><img src={logo} width="400"></img></p>
+    <p><img src={logo} width="400px" alt="mevivo logo"></img></p>
+    <p>Sekunden auf dieser Seite: {count}</p>
   </div>
-);
+  );
+}
 
 const MainContent = () => {
   return (
     <main className={styles.main}>
       <Routes>
-        <Route path="/" element={<Home />} >
-        </Route>
-        <Route path="/buildings" element={<Buildings />}>
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/buildings" element={<Buildings />} />
       </Routes>
     </main>
   );
